969371284a248978a6ae623b87b202487a70066b
mudit0829 <mekasutechnology@gmail.com>
2025-09-28 13:54:06 +0530

    Update profile.js

--- profile.js
+++ profile.js
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   // Logout
   document.getElementById("logoutBtn").onclick = () => {
     localStorage.clear();
-    window.location.href = "login.html";
+    window.location.href = "index.html";
   };
 
   // Navigation handlers
